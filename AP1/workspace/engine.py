1e5c11394f28354466e14017ba87c12766db01d5
Mariany Silva <mariany.silva@trustedhealth.com>
2022-05-07 13:32:34 -0300

    [AP1] docs: update solution explanation

---
+++ AP1/workspace/engine.py
@@ -0,0 +1,20 @@
+def amplitude_search():
+    pass
+
+def depth_search():
+    pass
+
+def calculate_cost():
+    pass
+
+def uniform_cost_search():
+    pass
+
+def simple_heuristic_search():
+    pass
+
+def complex_heuristic_search():
+    pass
+
+def print_result():
+    pass
